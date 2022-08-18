d05152392bd8bc4458f584377c9a0cc9624f4d76
shohagroy <pkshohag240@gmail.com>
2022-08-18 17:59:53 +0600

    js file added

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+console.log('hello world')
+
+const outPutScreen = document.getElementById('output-value');
+const outPutValue = outPutScreen.value;
+const outPutScreenNumber = parseFloat(outPutValue);
+console.log(outPutValue);
